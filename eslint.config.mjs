0281d793a34b11da89fda824c553efa30b2cd788
side-26 <mahdirashidi462@gmail.com>
2026-08-11 21:31:28 +0330

    initial the project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,11 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-plugin-prettier/recommended"; // Import Prettier config
 
 const eslintConfig = defineConfig([
   ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  prettierConfig, // Add Prettier config to the extends array
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
